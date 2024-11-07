c4b364d6b8b4bf341ed78fdb8f2aa8b69ad2b1bd
BedouiDalenda <dalenda.bedoui@etudiant-enit.utm.tn>
2024-11-07 22:18:56 +0100

    Add files via upload

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({   /* component is feature from angular we can use it */
+  selector: 'app-home',/*means you can use <app-home> in your HTML to show this component.*/
+  templateUrl: './home.component.html',/*points to the HTML file that contains the layout for this component.*/
+  styleUrls: ['./home.component.css'] ,/*lists the CSS files for styling this component.*/
+})
+export class HomeComponent {
+
+}
